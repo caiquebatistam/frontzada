dd04c15a29b48c60b18b8b7fa1ec81f6f701769b
caiquebatistam <88943229+caiquebatistam@users.noreply.github.com>
2022-06-15 00:08:59 -0300

    orgonizando os arq

--- JavaScript/FundamentosJS/fundamentos/letLoop.js
+++
@@ -1,8 +0,0 @@
-/* Neste ex, o let é apenas local, então fora do loop, ele não 
-   estara visivel*/
-
-for ( let i = 0; i <10; i++) {
-    console.log(i)
-}
-
-console.log('i = ', i)
--- JavaScript/FundamentosJS/fundamentos/variaveisEConstantes.js
+++
@@ -1,9 +0,0 @@
-var a = 3
-
-let b = 4
-
-const c = 5
-
-console.log(a, b, c)
-
-
